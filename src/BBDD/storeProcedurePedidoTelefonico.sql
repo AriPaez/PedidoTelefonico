2304624df74cf6772ef0cc46f22ab7af3ccc81c1
AriPaez <paezariel69@gmail.com>
2021-02-23 14:22:45 -0300

    registrarEmpleado

--- src/BBDD/storeProcedurePedidoTelefonico.sql
+++ src/BBDD/storeProcedurePedidoTelefonico.sql
@@ -1,6 +1,6 @@
 --Iniciar sesion 
 
-CREATE PROCEDURE iniciarSesion(@dni VARCHAR(8),@contrasenia VARCHAR(30))
+ALTER PROCEDURE iniciarSesion(@dni VARCHAR(8),@contrasenia VARCHAR(30))
 AS
 BEGIN TRY
 
@@ -31,3 +31,33 @@ BEGIN CATCH
 END CATCH
 
 --registrar empleado
+CREATE PROCEDURE registrarEmpleado(@dni VARCHAR(8),@contrasenia VARBINARY(MAX))
+AS
+BEGIN TRY
+
+	IF(LEN(@dni)<>8)
+	BEGIN
+		RAISERROR('EL NÚMERO DE DNI DEBE SER DE 8 DÍGITOS',14,1)
+	END
+	ELSE IF (ISNUMERIC(@contrasenia)=0)
+	BEGIN
+		RAISERROR('ERROR. EL NÚMERO DE DNI DEBE ENTERO',14,1)
+	END
+	ELSE IF EXISTS (SELECT dniEmpleado FROM empleado WHERE dniEmpleado=@dni
+	AND contrasenia=@contrasenia)
+	BEGIN
+		RAISERROR('ERROR. YA EXISTE EL DNI EN LA BBDD.',14,1)
+	END
+	ELSE
+	BEGIN
+	 INSERT INTO empleado
+			   (dniEmpleado
+			   ,contrasenia)
+		 VALUES(@dni,PWDENCRYPT(@contrasenia))
+	END
+END TRY
+BEGIN CATCH
+	DECLARE @mensajeDeError VARCHAR(100)
+	SELECT @mensajeDeError=ERROR_MESSAGE()
+	RAISERROR (@mensajeDeError,14,1)
+END CATCH
