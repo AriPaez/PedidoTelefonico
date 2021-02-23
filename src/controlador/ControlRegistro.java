956dc822d435eb8c48161dc063199eec7df9aeb3
AriPaez <paezariel69@gmail.com>
2021-02-23 16:00:13 -0300

    control registro

--- src/controlador/ControlRegistro.java
+++ src/controlador/ControlRegistro.java
@@ -3,12 +3,41 @@ package controlador;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import javax.swing.JFrame;
+
+import modelo.LoginRegistro;
+import vista.Login;
+import vista.Registro;
+
 public class ControlRegistro implements ActionListener {
 
+	private Registro registro;
+	private LoginRegistro loginRegistro;
+	
+	public ControlRegistro(Registro r)
+	{
+		registro=r;
+		loginRegistro=new LoginRegistro(r);
+	}
+	
 	@Override
 	public void actionPerformed(ActionEvent e) {
 	 
-
+		Object botonElegido=e.getSource();
+		
+		if(botonElegido==registro.getRegistrar())
+		{
+			
+		}
+		else
+		{
+			registro.dispose();
+			Login l=new Login();
+			l.setVisible(true);
+			l.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		}
+		
+		
 	}
 
 }
