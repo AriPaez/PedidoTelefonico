d25f814f00d2667ae9fb308226af9d63522ababe
AriPaez <paezariel69@gmail.com>
2021-02-26 15:07:21 -0300

    control cliente

--- src/controlador/ControlCliente.java
+++ src/controlador/ControlCliente.java
@@ -7,10 +7,11 @@ import vista.Cliente;
 public class ControlCliente implements ActionListener {
 
 	private Cliente cliente;
-	private ABMCliente aBMCliente;
+	//private ABMLCliente aBMLCliente;
 	
 	public ControlCliente(Cliente c)
 	{
+		//aBMLCliente=new ABMLCliente(c);
 		cliente=c;
 	}
 	 
@@ -18,7 +19,34 @@ public class ControlCliente implements ActionListener {
 	@Override
 	public void actionPerformed(ActionEvent e) {
 	 
-
+		Object botonElegido=e.getSource();
+		
+		if(botonElegido==cliente.getRegistrarRegistro())
+		{
+			
+		}
+		else if(botonElegido==cliente.getRegistrarActualizacion())
+		{
+			
+		}
+		else if(botonElegido==cliente.getRegistrarEliminacion())
+		{
+			
+		}
+		else if(botonElegido==cliente.getCancelarRegistro())
+		{
+			
+		}
+		else if(botonElegido==cliente.getCancelarActualizacion())
+		{
+			
+		}
+		else if(botonElegido==cliente.getCancelarEliminacion())
+		{
+			
+		}
+		
+		
 	}
 
 }
